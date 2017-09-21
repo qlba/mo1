493e69325c116f4a4de0d3d550d445c6537b4c87
qlba <qlba@ya.ru>
2017-09-21 20:58:24 +0300

    Brute force simplex method

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var {Mx} = require('./frac_mx/mx'),
 	{Frac} = require('./frac_mx/frac'),
 	{combinations} = require('./utils/combinatorics');
 
+// var mat = [
+// 	[5,  5, -3,  1, -1,  4],
+// 	[2,  1, -2,  3,  4, -1],
+// 	[1,  5, -6,  3,  2,  4],
+// 	// [4,  2, -4,  6,  8, -2],
+// 	[0, -1, -1, -1, -1, -1]
+// ];
+
 var mat = [
-	[18,  2, 1, -3, 0, 0,  6],
-	[24, -3, 0,  2, 1, 0, -2],
-	[36,  1, 0,  3, 0, 1, -4],
-	[ 0, -3, 0, -2, 0, 0,  6]
+	[5, -3,  1, -1,  4, 5],
+	[1, -2,  3,  4, -1, 2],
+	[5, -6,  3,  2,  4, 1]
 ];
 
 var mx = new Mx(mat.length, mat[0].length);
@@ -16,15 +23,19 @@ mx.fill(function(_, i, j)
 	return new Frac(mat[i][j], 1);
 });
 
-combinations(3, 1, 6).forEach(function(combination)
+// mx.makeBasic(1, 1);
+// mx.makeBasic(0, 2);
+// mx.makeBasic(2, 3);
+
+console.log(mx.toString());
+
+combinations(3, 1, 5).forEach(function(combination)
 {
 	try
 	{
+		console.log(combination);
+
 		mx.toBasicView(combination);
-		
-		for(var i = 0; i < 3; i++)
-			if(mx.getElement(i, 0).less(mx.getElement(i, 0).neg()))
-				return;
 
 		var point = [];
 
@@ -41,6 +52,13 @@ combinations(3, 1, 6).forEach(function(combination)
 		console.log('[' + point.join(', ') + ']: Z = ' +
 			mx.getElement(3, 0).toString('%.0f')
 		);
+		
+		for(var i = 0; i < 3; i++)
+			if(mx.getElement(i, 0).less(mx.getElement(i, 0).neg()))
+				console.log('NOT ACCEPTABLE');
+
+		console.log(mx.toString());
+
 	}
 	catch(ex)
 	{
