e804371732eead032fc9043d47971c0b0b08a23d
qlba <qlba@ya.ru>
2017-11-24 22:01:03 +0300

    Password reader

--- tafl_qrsa4/index.js
+++ tafl_qrsa4/index.js
@@ -1,21 +1,67 @@
-var stdin = process.stdin;
+var readline = require('readline');
 
-// without this, we would only get streams once enter is pressed
-stdin.setRawMode(true);
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
-// resume stdin in the parent process (node app won't quit all by itself
-// unless an error or process.exit() happens)
-stdin.resume();
+function hidden(query, callback) {
+	var stdin = process.openStdin(),
+		i = 0;
+	process.stdin.on("data", function (char) {
+		char = char + "";
+		switch (char) {
+			case "\n":
+			case "\r":
+			case "\u0004":
+				stdin.pause();
+				break;
+			default:
+				process.stdout.write("\033[2K\033[200D" + query + "[" + ((i % 2 == 1) ? "=-" : "-=") + "]");
+				i++;
+				break;
+		}
+	});
 
-// i don't want binary, do you?
-stdin.setEncoding('utf8');
+	rl.question(query, function (value) {
+		rl.history = rl.history.slice(1);
+		callback(value);
+	});
+}
 
-// on any data into stdin
-stdin.on('data', function (key) {
-	// ctrl-c ( end of text )
-	if (key === '\u0003') {
-		process.exit();
-	}
-	// write the key to stdout all normal like
-	console.log(key.charCodeAt(0));
+hidden("password : ", function (password) {
+	console.log("Your password : " + password);
+});
+
+var readline = require('readline');
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+function hidden(query, callback) {
+	var stdin = process.openStdin();
+	process.stdin.on("data", function (char) {
+		char = char + "";
+		switch (char) {
+			case "\n":
+			case "\r":
+			case "\u0004":
+				stdin.pause();
+				break;
+			default:
+				process.stdout.write("\033[2K\033[200D" + query + Array(rl.line.length + 1).join("*"));
+				break;
+		}
+	});
+
+	rl.question(query, function (value) {
+		rl.history = rl.history.slice(1);
+		callback(value);
+	});
+}
+
+hidden("password : ", function (password) {
+	console.log("Your password : " + password);
 });
