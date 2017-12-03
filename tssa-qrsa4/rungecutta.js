cc0d859daa2ce2f7395aa66300371cf152d94190
qlba <qlba@ya.ru>
2017-12-03 22:36:28 +0300

    wip

--- tssa-qrsa4/rungecutta.js
+++ tssa-qrsa4/rungecutta.js
@@ -3,9 +3,9 @@ function rungecutta(fs, y0s, xs)
 	const n = xs.length;
 	const m = fs.length;
 	
-	const ys = new Array(m).fill(0).map(() => new Array(n));
+	const ys = new Array(n).fill(0).map(() => new Array(m));
 
-	y0s.forEach((y0, j) => ys[j][0] = y0);
+	y0s.forEach((y0, j) => ys[0][j] = y0);
 
 
 	const ks = new Array(m).fill(0).map(() => new Array(4));
@@ -15,19 +15,19 @@ function rungecutta(fs, y0s, xs)
 		const h = xs[i] - xs[i - 1];
 
 		for (let j = 0; j < m; j++)
-			ks[j][0] = fs[j](xs[i - 1], ...ys.map(vs => vs[i - 1]));
+			ks[j][0] = fs[j](xs[i - 1], ...ys[i - 1]);
 
 		for (let j = 0; j < m; j++)
-			ks[j][1] = fs[j](xs[i - 1] + h / 2, ...ys.map((vs, j) => vs[i - 1] + ks[j][0] * h / 2));
+			ks[j][1] = fs[j](xs[i - 1] + h / 2, ...ys[i - 1].map((y, j) => y + ks[j][0] * h / 2));
 
 		for (let j = 0; j < m; j++)
-			ks[j][2] = fs[j](xs[i - 1] + h / 2, ...ys.map((vs, j) => vs[i - 1] + ks[j][1] * h / 2));
+			ks[j][2] = fs[j](xs[i - 1] + h / 2, ...ys[i - 1].map((y, j) => y + ks[j][1] * h / 2));
 
 		for (let j = 0; j < m; j++)
-			ks[j][3] = fs[j](xs[i], ...ys.map((vs, j) => vs[i - 1] + ks[j][2] * h));
+			ks[j][3] = fs[j](xs[i], ...ys[i - 1].map((y, j) => y + ks[j][2] * h));
 
 		for (let j = 0; j < m; j++)
-			ys[j][i] = ys[j][i - 1] +
+			ys[i][j] = ys[i - 1][j] +
 				h * (ks[j][0] + 2 * ks[j][1] + 2 * ks[j][2] + ks[j][3]) / 6;
 	}
 
@@ -35,16 +35,42 @@ function rungecutta(fs, y0s, xs)
 }
 
 
+const printf = require('printf');
 const _ = require('lodash');
 
-console.time('calc');
-const ys = rungecutta([
-	(x, y1, y2, y3, y4) => y2,
-	(x, y1, y2, y3, y4) => y3,
-	(x, y1, y2, y3, y4) => y4,
-	(x, y1, y2, y3, y4) => 1
-],
-[0, 0, 0, 0], _.range(0, 3, 0.00003));
-console.timeEnd('calc');
+const EARTH_RADIUS = 6378165;
+const EARTH_GRAVITY = -9.8 * EARTH_RADIUS * EARTH_RADIUS;
+const X0 = -4610050;
+const Y0 = 4710000;
+const VX0 = 5000;
+const VY0 = 6000;
 
-console.log(ys.map(vs => _.last(vs)));
+const dx = (t, x, y, vx, vy) => vx;
+const dy = (t, x, y, vx, vy) => vy;
+const dvx = (t, x, y, vx, vy) => EARTH_GRAVITY * x / Math.pow(x * x + y * y, 3 / 2);
+const dvy = (t, x, y, vx, vy) => EARTH_GRAVITY * y / Math.pow(x * x + y * y, 3 / 2);
+
+
+logPosition(0, X0, Y0);
+
+for (let i = 0, init = [X0, Y0, VX0, VY0]; i < 1 * 60; i++)
+{
+	const ys = rungecutta([dx, dy, dvx, dvy], init, _.range(0, 60, 0.01));
+
+	logPosition(i + 1, _.last(ys)[0], _.last(ys)[1]);
+
+	init = _.last(ys);
+}
+
+function logPosition(minute, XH, YH)
+{
+	console.log(
+		printf('%2d:%02d', _.floor(minute / 60), minute % 60).padStart(6) + ' ' +
+		(
+			Math.round(Math.atan2(YH, XH) * 180 / Math.PI)
+		).toString().padStart(25) + '\u00B0 ' +
+		(
+			Math.round((Math.sqrt(XH * XH + YH * YH) - EARTH_RADIUS) / 1000)
+		).toString().padStart(25) + ' km'
+	);
+}
